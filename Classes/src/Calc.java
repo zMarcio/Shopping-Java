5fb91e1984c7a372765610c65bb11cf044133381
MÁRCIO PAULO DE ALMEIDA LIMA <113918441+zMarcio@users.noreply.github.com>
2024-09-05 15:50:42 -0300

    Delete Classes directory

--- Classes/src/Calc.java
+++
@@ -1,5 +0,0 @@
-public class Calc {
-    public String calcTotalTempo(Titulo t){
-        return "Nome do Título: " + t.getNome() + ", Duração: " + t.getDuracao()/60 + " horas e " + t.getDuracao()%60 + " minutos";
-    }
-}
